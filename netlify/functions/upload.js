f0568d1c4768721b434a6a3d270eb0ffc375987c
blazittx <info@doruksasmaz.com>
2023-11-01 18:03:33 +0300

    Fix

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -1,72 +1,71 @@
 const { PrismaClient } = require('@prisma/client');
 const axios = require('axios');
 
-
 const prisma = new PrismaClient();
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
-
-  const data = JSON.parse(event.body);
-
-  // Get the username from the request (assuming you're passing it in the request)
-  const username = data.username;
-
-  // Identify the team of the user
-  const teamName = getTeamForUsername(username);
-  if (!teamName) {
-    return {
-      statusCode: 403,  // Forbidden
-      body: JSON.stringify({ error: 'User is not authorized to upload for any team.' }),
-    };
-  }
-
-  try {
-    // Check if a card for that team already exists
-    const existingCard = await prisma.card.findUnique({
-      where: { teamName: teamName }
-    });
-
-    let result;
-    if (existingCard) {
-      // Update the existing card
-      result = await prisma.card.update({
-        where: { teamName: teamName },
-        data: data
-      });
-    } else {
-      // Create a new card
-      result = await prisma.card.create({ data: data });
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
     }
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Data uploaded successfully', result: result }),
-    };
+    const data = JSON.parse(event.body);
 
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
+    // Get the username from the request (assuming you're passing it in the request)
+    const username = data.username;
+
+    // Identify the team of the user
+    const teamName = await getTeamForUsername(username);  // <-- Added 'await' here
+    if (!teamName) {
+        return {
+            statusCode: 403,  // Forbidden
+            body: JSON.stringify({ error: 'User is not authorized to upload for any team.' }),
+        };
+    }
+
+    try {
+        // Check if a card for that team already exists
+        const existingCard = await prisma.card.findUnique({
+            where: { teamName: teamName }
+        });
+
+        let result;
+        if (existingCard) {
+            // Update the existing card
+            result = await prisma.card.update({
+                where: { teamName: teamName },
+                data: data
+            });
+        } else {
+            // Create a new card
+            result = await prisma.card.create({ data: data });
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Data uploaded successfully', result: result }),
+        };
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
 };
 
 async function getTeamForUsername(username) {
     try {
-      const response = await axios.get('https://diabolical.services/authorized_users.json');
-      const teamAssignments = response.data;
-  
-      for (const [team, users] of Object.entries(teamAssignments)) {
-        if (users.includes(username)) {
-          return team;
+        const response = await axios.get('https://diabolical.services/authorized_users.json');
+        const teamAssignments = response.data;
+
+        for (const [team, users] of Object.entries(teamAssignments)) {
+            if (users.includes(username)) {
+                return team;
+            }
         }
-      }
     } catch (error) {
-      console.error('Error fetching team assignments:', error);
-      throw error;
+        console.error('Error fetching team assignments:', error);
+        throw error;
     }
     return null; // Return null if the username doesn't belong to any team
-  }
+}
