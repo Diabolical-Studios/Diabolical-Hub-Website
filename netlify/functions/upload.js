d3a53e71f3ad4de41d2e1233323be3e922d2a14f
blazittx <info@doruksasmaz.com>
2023-11-01 11:51:37 +0300

    New Trial

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -1,64 +1,30 @@
-// upload.js
-const Busboy = require('busboy');
-const fs = require('fs');
+// netlify/functions/upload.js
+
+const fs = require('fs').promises;
 const path = require('path');
 
-exports.handler = async function (event, context) {
-    // Check for POST method
-    if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
-    }
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
 
-    const contentType = event.headers['content-type'];
-    if (!contentType) {
-        return { statusCode: 400, body: 'Invalid content type' };
-    }
+  const data = JSON.parse(event.body);
+  
+  // Path to your data.json
+  const dataFilePath = path.join(__dirname, '../data.json');
 
-    return new Promise((resolve, reject) => {
-        const busboy = new Busboy({ headers: { 'content-type': contentType } });
+  // Load existing data
+  const rawData = await fs.readFile(dataFilePath, 'utf-8');
+  const jsonData = JSON.parse(rawData);
+  
+  // Push new card data
+  jsonData.cards.push(data);
 
-        let fields = {};
-        let files = {};
+  // Save back the data
+  await fs.writeFile(dataFilePath, JSON.stringify(jsonData));
 
-        busboy.on('field', (fieldname, value) => {
-            fields[fieldname] = value;
-        });
-
-        busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-            const uploadPath = path.join(__dirname, 'uploads', filename);
-            file.pipe(fs.createWriteStream(uploadPath));
-            files[fieldname] = { filename, encoding, mimetype };
-        });
-
-        busboy.on('finish', () => {
-            // Destructure form fields
-            const { teamName, gameIcon, gameBanner, gameName, gameDescription } = fields;
-
-            // Construct card data object
-            const cardData = { teamName, gameIcon, gameBanner, gameName, gameDescription };
-
-            // Resolve file path to data.json
-            const dataFilePath = path.resolve(__dirname, '../data.json');
-
-            // Read existing data from data.json
-            const existingData = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
-
-            // Add new card data to existing data
-            existingData.cards.push(cardData);
-
-            // Write updated data back to data.json
-            fs.writeFileSync(dataFilePath, JSON.stringify(existingData));
-
-            // Resolve promise with success response
-            resolve({
-                statusCode: 200,
-                body: JSON.stringify({ success: true }),
-            });
-        });
-
-        busboy.on('error', error => reject({ statusCode: 500, body: JSON.stringify(error) }));
-
-        busboy.write(event.body, 'binary');
-        busboy.end();
-    });
-};
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Data added successfully!" }),
+  };
+}
